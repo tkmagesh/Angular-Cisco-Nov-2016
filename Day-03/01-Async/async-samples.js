c1d5da624a7c1eb4a41d156a2c1c677d1018e5ad
Magesh <tkmagesh77@gmail.com>
2016-11-11 14:26:53 +0530

    JSON file for Server communication

--- Day-03/01-Async/async-samples.js
+++ Day-03/01-Async/async-samples.js
@@ -12,7 +12,7 @@ var pgm = (function(){
 		console.log('[@Consumer] result = ', result);
 	}
 
-	function addAsync(x,y, onResult){
+	function addAsyncCallback(x,y, onResult){
 		console.log('	[@Service] processing ', x ,' and ', y);
 		setTimeout(function(){
 			var result = x + y;
@@ -23,16 +23,29 @@ var pgm = (function(){
 	}
 
 	function addAsyncClient(x,y){
-		console.log('[@Consumer] triggering addAsync');
-		addAsync(x,y, function(result){
+		console.log('[@Consumer] triggering addAsyncCallback');
+		addAsyncCallback(x,y, function(result){
 			console.log('[@Consumer] result = ', result);	
 		});
 		
 	}
 
+	function addAsyncPromise(x,y){
+		console.log('	[@Service] processing ', x ,' and ', y);
+		var promise = new Promise(function(resolveFn, rejectFn){
+			setTimeout(function(){
+				var result = x + y;
+				console.log('	[@Service] returning result');
+				resolveFn(result);
+			}, 5000);
+		});
+		return promise;
+	}
+
 	return {
 		addSyncClient : addSyncClient,
-		addAsyncClient : addAsyncClient 
+		addAsyncClient : addAsyncClient,
+		addAsyncPromise : addAsyncPromise
 	}
 
 })();
